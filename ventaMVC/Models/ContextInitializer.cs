a2fbb36e919c07f57a8b1087c4ac108f761876da
ramons03 <ramons_03@hotmail.com>
2012-03-01 16:29:07 -0300

    db372d76f30e104e10b652a007012fc0f8

--- ventaMVC/Models/ContextInitializer.cs
+++ ventaMVC/Models/ContextInitializer.cs
@@ -10,7 +10,7 @@ namespace ventaMVC.Models
     {
         public static void Init()
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
         }
     }
 }
